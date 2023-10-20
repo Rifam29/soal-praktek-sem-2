307cd85fe57e8f1090ca37ff9fa6fa0526a23ec9
Rifam29 <rifamazharulhaq.dhp@gmail.com>
2023-10-20 23:07:26 +0700

    Ubah

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 list_cash_flow = [
   2500000, 5000000, -1000000, -2500000, 5000000, 10000000,
   -5000000, 7500000, 10000000, -1500000, 25000000, -2500000
-  ]
-  total_pengeluaran = ...
-  total_pemasukan   = ...
-  for (let x in ... )
-  {
-    if (list_cash_flow[...] > 0)
-    {
-      // Jumlahkan bilangan positif (total pemasukan)
-      .. += list_cash_flow[...]
-    }
-    else
-    {
-      // jumlahkan bilangan negarif (total pengeluaran)
-      .. += ...[x]
-    }
-total_pengeluaran *= -1
+];
+
+total_pengeluaran = 0;
+total_pemasukan = 0;
+
+for (let x in list_cash_flow) {
+  if (list_cash_flow[x] > 0) {
+    // Jumlahkan bilangan positif (total pemasukan)
+    total_pemasukan += list_cash_flow[x];
+  } else {
+    // jumlahkan bilangan negarif (total pengeluaran)
+    total_pengeluaran += list_cash_flow[x];
   }
-  console.log(`Total Pengeluaran adalah : ${...}`) 
-  console.log(`Total Pemasukan adalah : ${...}`)
+}
+
+// total_pengeluaran *= -1 untuk mengubah tanda dari bilangan negatif menjadi positif
+total_pengeluaran *= -1;
+
+console.log(`Total Pengeluaran adalah : ${total_pengeluaran}`);
+console.log(`Total Pemasukan adalah : ${total_pemasukan}`);
